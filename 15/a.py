d93ad6d2ba87a9dd685b70029d12edcaf7be61ca
Andrew E. Timmes <andrew.timmes@gmail.com>
2022-12-15 12:40:49 -0500

    Day 15 and boy oh boy is this bad

--- 15/a.py
+++ 15/a.py
@@ -4,16 +4,61 @@ from aocd import get_data, submit
 from aocd.transforms import lines
 
 YEAR = 2022
-DAY = FIXME
+DAY = 15
 PART = "a"
+
+SCAN_LINE = 2_000_000
+#SCAN_LINE = 10
+
+DATA = """Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3"""
 
 
 def main():
     """Part a."""
     data = lines(get_data(day=DAY, year=YEAR))
-    print(f"{data=}")
+    #data = DATA.splitlines()
+    print("\n".join(data))
 
-    result = 0
+    sensors = {}
+
+    for line in data:
+        tokens = line.split()
+        sx, sy, bx = map(int, [tokens[i][2:-1] for i in [2, 3, -2]])
+        by = int(tokens[-1][2:])
+        sensors[(sx, sy)] = (bx, by)
+
+    seen = set()
+    for (sx, sy), (bx, by) in sensors.items():
+        mh_dist = abs(bx - sx) + abs(by - sy)
+
+        const_mh_dist = abs(sy - SCAN_LINE)
+
+        delta_dist = abs(mh_dist - const_mh_dist)
+
+        for i in range(delta_dist + 1):
+            seen.add(sx + i)
+            seen.add(sx - i)
+
+    for (sx, sy), (bx, by) in sensors.items():
+        if sy == SCAN_LINE and sx in seen:
+            seen.remove(sx)
+        if by == SCAN_LINE and bx in seen:
+            seen.remove(bx)
+
+    result = len(seen)
 
     print(f"{result=}")
     submit(result, part=PART, day=DAY, year=YEAR)
---
+++ 15/b.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3.10
+"""2022 day FIXME."""
+from aocd import get_data, submit
+from aocd.transforms import lines
+
+YEAR = 2022
+DAY = 15
+PART = "b"
+
+SCAN_BOUND = 4000000
+
+
+def bounds_check(x, y):
+    """Check if coords are in-bounds."""
+    return x >= 0 and y >= 0 and x <= SCAN_BOUND and y <= SCAN_BOUND
+
+
+def mh_dist(x1, y1, x2, y2):
+    """Calculate manhattan distance between two points."""
+    return abs(x2 - x1) + abs(y2 - y1)
+
+
+def main():
+    """Part b."""
+    data = lines(get_data(day=DAY, year=YEAR))
+    print("\n".join(data))
+
+    sensors: dict[tuple[int, int], int] = {}
+
+    for line in data:
+        tokens = line.split()
+        sx, sy, bx = map(int, [tokens[i][2:-1] for i in [2, 3, -2]])
+        by = int(tokens[-1][2:])
+        sensors[(sx, sy)] = mh_dist(sx, sy, bx, by)
+        print(f"{sx=} {sy=} {bx=} {by=} {mh_dist(sx,sy,bx,by)=}")
+
+    candidates: dict[tuple[int, int], int] = {}
+
+    #    Consider x=5,y=5 with mhd = 2:
+    # 123456789
+    # 1
+    # 2
+    # 3    #
+    # 4   ###
+    # 5  ##S#B
+    # 6   ###
+    # 7    #
+    # 8
+    # 9
+    #    we want to start at x +/- (mhd+1), y and iterate (mhd+1) times per side
+
+    for (sx, sy), mhd in sensors.items():
+        for i in range(mhd + 2):
+            d1, d2 = i, (mhd + 1 - i)
+
+            for s1 in [1, -1]:
+                for s2 in [1, -1]:
+                    t = (sx + (d1 * s1), sy + (d2 * s2))
+                    candidates[t] = candidates.get(t, 0) + 1
+
+    result = -1
+
+    for cx, cy in sorted(candidates, key=lambda x: candidates[x], reverse=True):
+        failed = False
+        for (sx, sy), mhd in sensors.items():
+            if not failed and mh_dist(cx, cy, sx, sy) <= mhd:
+                failed = True
+        if not failed:
+            result = 4_000_000 * cx + cy
+            break
+
+    print(f"{result=}")
+    if result is not -1:
+        submit(result, part=PART, day=DAY, year=YEAR)
+
+
+if __name__ == "__main__":
+    main()
