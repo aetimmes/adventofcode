955861a9b13de078aa8e91358961f7b3ee0dec9d
Andrew E. Timmes <andrew.timmes@gmail.com>
2022-12-22 17:32:53 -0500

    22b WIP

---
+++ 22/b.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3.10
+"""2022 day 22."""
+from aocd import get_data, submit
+from aocd.transforms import lines
+
+YEAR = 2022
+DAY = 22
+PART = "b"
+
+DATA = """        ...#
+        .#..
+        #...
+        ....
+...#.......#
+........#...
+..#....#....
+..........#.
+        ...#....
+        .....#..
+        .#......
+        ......#.
+
+10R5L5R10L4R5L5"""
+
+# down/right/clockwise is positive, deltas[0] is right
+deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+glyphs = [">", "v", "<", "^"]
+turns = {"R": 1, "L": -1}
+
+
+def main():
+    """Part b."""
+    #data = get_data(day=DAY, year=YEAR)
+    data = DATA
+
+    grid, instructions = data.split("\n\n")
+    print(grid)
+    grid = lines(grid)
+    open_tiles = set()
+    walls = set()
+
+    max_r = len(grid)
+    max_c = max(len(line) for line in grid)
+
+    position: tuple[int, int] = (0, grid[0].index("."))
+    facing: int = 0
+
+    for r, line in enumerate(grid):
+        for c, char in enumerate(line):
+            if char == ".":
+                open_tiles.add((r, c))
+            elif char == "#":
+                walls.add((r, c))
+
+    result = 0
+
+    current = ""
+    for i, c in enumerate(instructions):
+        if c in ["L", "R"]:
+            position = process_steps(
+                open_tiles, walls, max_r, max_c, position, facing, current
+            )
+            facing = process_facing(facing, c)
+            current = ""
+        else:
+            current += c
+
+    if current:
+        position = process_steps(
+            open_tiles, walls, max_r, max_c, position, facing, current
+        )
+
+    result = ((position[0] + 1) * 1000) + ((position[1] + 1) * 4) + facing
+    print(f"{result=}")
+    #submit(result, part=PART, day=DAY, year=YEAR)
+
+
+def process_steps(open_tiles, walls, max_r, max_c, position, facing, current):
+    """Take a walk on a cubular monkey forcefield."""
+    steps = int(current)
+    for _ in range(steps):
+        temp = (
+            (position[0] + deltas[facing][0]),
+            (position[1] + deltas[facing][1]),
+        )
+        if temp not in open_tiles and temp not in walls:
+            process_warp(position, facing, open_tiles, walls, max_r, max_c)
+        if temp in open_tiles:
+            position = temp
+        if temp in walls:
+            break
+    return position
+
+def process_warp(position, facing, open_tiles, walls, max_r, max_c):
+
+
+def process_facing(facing, c):
+    """Turn! Turn! Turn! (To everything there is a facing)."""
+    facing = (facing + turns[c]) % len(deltas)
+    return facing
+
+
+if __name__ == "__main__":
+    main()
