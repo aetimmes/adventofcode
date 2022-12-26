459c4c5afe701dbcce4f57e0fa7b6c732524ab4e
Andrew E. Timmes <andrew.timmes@gmail.com>
2022-12-25 23:13:47 -0500

    2015d8 solution

--- src/y2015/d08/b.py
+++ src/y2015/d08/b.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3.11
 """2015 day 8."""
+import re
+
 from aocd import get_data, submit
 from aocd.transforms import lines
 
 YEAR = 2015
 DAY = 8
-PART = "a"
+PART = "b"
 
 
 def main():
-    """Part a."""
+    """Part b."""
     data = lines(get_data(day=DAY, year=YEAR))
     print(f"{data=}")
 
     result = 0
     for line in data:
-        result += len(line) - len(eval(line))  # pylint: disable=eval-used
+        newline = re.sub(r"([\\\"])", r"\\\1", line)
+        print(f"{line=}, {len(line)=}, {newline=} {len(newline) + 2=}")
+        result += len(newline) - len(line) + 2
 
     print(f"{result=}")
     submit(result, part=PART, day=DAY, year=YEAR)
