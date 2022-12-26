a9e09a4f602f6174ba3daf70612786382f82bd98
Andrew E. Timmes <andrew.timmes@gmail.com>
2022-12-26 12:59:23 -0500

    2015d17 solution

--- src/y2015/d17/b.py
+++ src/y2015/d17/b.py
@@ -5,31 +5,35 @@ from aocd.transforms import lines
 
 YEAR = 2015
 DAY = 17
-PART = "a"
+PART = "b"
 
 TARGET = 150
 
 
 def main():
-    """Part a."""
+    """Part b."""
     data = lines(get_data(day=DAY, year=YEAR))
     print(f"{data=}")
 
     sizes_ = tuple(map(int, data))
     counts_ = tuple()
-    solutions = set()
+    solutions = [set() for _ in sizes_]
 
     def descent(target, index, sizes, counts):
         """DFS to get sums."""
         if index == len(sizes):
             if target == 0:
-                solutions.add(counts)
+                solutions[sum(counts)].add(counts)
             return
         for i in [0, 1]:
             descent(target - sizes[index] * i, index + 1, sizes, counts + (i,))
 
     descent(TARGET, 0, sizes_, counts_)
-    result = len(solutions)
+    result = -1
+    for _, s in enumerate(solutions):
+        if s:
+            result = len(s)
+            break
     print(f"{result=}")
     submit(result, part=PART, day=DAY, year=YEAR)
 
