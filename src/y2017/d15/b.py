1a2b1eac237332c77bd1616930320cde4b835125
Andrew E. Timmes <andrew.timmes@gmail.com>
2023-01-01 21:25:31 -0500

    2017 d15-16 solutions

--- src/y2017/d15/b.py
+++ src/y2017/d15/b.py
@@ -5,7 +5,7 @@ from aocd.transforms import lines
 
 YEAR = 2017
 DAY = 15
-PART = "a"
+PART = "b"
 
 A = 16807
 B = 48271
@@ -13,23 +13,29 @@ DIVISOR = 2147483647
 
 
 def main():
-    """Part a."""
+    """Part b."""
     data = lines(get_data(day=DAY, year=YEAR))
     print(f"{data=}")
 
     a, b = (int(line.split()[-1]) for line in data)
 
     result = 0
-    for _ in range(40_000_000):
-        a *= A
-        b *= B
-        a %= DIVISOR
-        b %= DIVISOR
+    for _ in range(5_000_000):
+        a = gen(a, A, 4)
+        b = gen(b, B, 8)
         if f"{a:016b}"[-16:] == f"{b:016b}"[-16:]:
             result += 1
 
     print(f"{result=}")
     submit(result, part=PART, day=DAY, year=YEAR)
+
+
+def gen(val, mult, mod):
+    """Generate the next number."""
+    val = (val * mult) % DIVISOR
+    while val % mod != 0:
+        val = (val * mult) % DIVISOR
+    return val
 
 
 if __name__ == "__main__":
--- src/y2017/d16/b.py
+++ src/y2017/d16/b.py
@@ -4,16 +4,30 @@ from aocd import get_data, submit
 
 YEAR = 2017
 DAY = 16
-PART = "a"
+PART = "b"
 
 
 def main():
-    """Part a."""
+    """Part b."""
     data = get_data(day=DAY, year=YEAR).split(",")
     print(f"{data=}")
 
     elements = [chr(ord("a") + i) for i in range(16)]
 
+    seen = {}
+    for i in range(1_000_000_000):
+        elements = dance(data, elements)
+        current = "".join(elements)
+        if current in seen:
+            break
+        seen[current] = i
+    result = [k for k, v in seen.items() if v == (1_000_000-1) % len(seen)][0]
+    print(f"{result=}")
+    submit(result, part=PART, day=DAY, year=YEAR)
+
+
+def dance(data, elements):
+    """Do a program dance."""
     for line in data:
         if line[0] == "s":
             elements = [elements[i - int(line[1:])] for i in range(16)]
@@ -24,9 +38,7 @@ def main():
             a, b = line[1:].split("/")
             x, y = elements.index(a), elements.index(b)
             elements[x], elements[y] = elements[y], elements[x]
-    result = "".join(elements)
-    print(f"{result=}")
-    submit(result, part=PART, day=DAY, year=YEAR)
+    return elements
 
 
 if __name__ == "__main__":
