b4fe39270b4227a9d1d1ec128f6eae3c9fdda274
Andrew E. Timmes <atimmes@seatgeek.com>
2023-12-08 10:49:40 -0500

    streamlining b

--- src/y2023/d08/b.py
+++ src/y2023/d08/b.py
@@ -3,11 +3,12 @@
 from aocd import get_data, submit
 import re
 from math import lcm
+
 YEAR = 2023
 DAY = 8
 PART = "b"
 
-DIRECTIONS = "LR"
+DIRECTIONS = {"L": 0, "R": 1}
 
 
 def main():
@@ -15,17 +16,14 @@ def main():
     chunks = get_data(day=DAY, year=YEAR).split("\n\n")
 
     steps = chunks[0].strip()
-    m = {}
-    for line in chunks[1].split("\n"):
-        n, l, r = re.findall('[A-Z][A-Z][A-Z]', line, re.DOTALL)
-        m[n] = [l, r]
+    m = {n: [l, r] for n, l, r in (re.findall("[A-Z][A-Z][A-Z]", line, re.DOTALL) for line in chunks[1].split("\n"))}
 
     lengths = []
-    for x in [x for x in m.keys() if x[2] == 'A']:
+    for x in filter(lambda x: x[2] == "A", m):
         i = 0
         current = x
-        while current[2] != 'Z':
-            current = m[current][DIRECTIONS.index(steps[i % len(steps)])]
+        while current[2] != "Z":
+            current = m[current][DIRECTIONS[(steps[i % len(steps)])]]
             i += 1
         lengths.append(i)
 
