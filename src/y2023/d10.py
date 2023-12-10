3c10dc789c8f72e082f3ece1cfc6897b1fef5d3a
Andrew E. Timmes <atimmes@seatgeek.com>
2023-12-10 12:50:34 -0500

    linting

--- src/y2023/d10.py
+++ src/y2023/d10.py
@@ -30,7 +30,7 @@ REPLACEMENTS = {
 
 
 def boundscheck(x, y, mx, my):
-    return (x >= 0 and x < mx and y >= 0 and y < my)
+    return x >= 0 and x < mx and y >= 0 and y < my
 
 
 def expand_graph(lines, loop):
@@ -93,14 +93,18 @@ def main():
             c = deck.pop()
             outside.add(c)
             for d in ((1, 0), (0, 1), (-1, 0), (0, -1)):
-                nr, nc = (c[0]+d[0], c[1]+d[1])
-                if boundscheck(nr, nc, len(graph), len(graph)) and graph[nr][nc] != "#" and (nr, nc) not in outside:
+                nr, nc = (c[0] + d[0], c[1] + d[1])
+                if (
+                    boundscheck(nr, nc, len(graph), len(graph))
+                    and graph[nr][nc] != "#"
+                    and (nr, nc) not in outside
+                ):
                     deck.append((nr, nc))
 
         result = 0
         for r in range(1, len(graph), 3):
             for c in range(1, len(graph), 3):
-                if (r, c) not in outside and graph[r][c] != '#':
+                if (r, c) not in outside and graph[r][c] != "#":
                     result += 1
 
         return result
@@ -110,17 +114,22 @@ def main():
         for i, ex in enumerate(puzzle.examples):
             try:
                 answer = locals()[part](ex.input_data)
-                if (
-                    (str(answer) != ex.getattr(f"answer_{part}")) and
-                    input(f"Example data mismatch: {ex.getattr(f'answer_{part}')=}, {answer=}; submit anyways? [y/N]:").lower().strip() != "y"
-                ):
+                if (str(answer) != ex.getattr(f"answer_{part}")) and input(
+                    f"Example data mismatch: {ex.getattr(f'answer_{part}')=}, {answer=}; submit anyways? [y/N]:"
+                ).lower().strip() != "y":
                     exit()
             except Exception as e:
                 print("Example data exception: %s" % str(e))
-                if input("Example data exception; submit anyways? [y/N]:").lower().strip() != "y":
+                if (
+                    input("Example data exception; submit anyways? [y/N]:")
+                    .lower()
+                    .strip()
+                    != "y"
+                ):
                     raise
 
         setattr(puzzle, f"answer_{part}", locals()[part](puzzle.input_data))
+
 
 if __name__ == "__main__":
     main()
