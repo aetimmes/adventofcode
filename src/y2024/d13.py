0b861a298b2a2c5680e92509550e52dc71f2e2b3
Andrew E. Timmes <andrew.timmes@gmail.com>
2024-12-15 09:41:13 -0500

    24 d13 answer

--- src/y2024/d13.py
+++ src/y2024/d13.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """2024 day 13."""
+import re
+from z3 import *
 from aocd.models import Puzzle
 
 YEAR = 2024
 DAY = 13
 
+C = 10000000000000
 
 def main():
     """Main."""
@@ -12,10 +15,40 @@ def main():
     def a(data):
         """Part a."""
         print('\n'.join(data.splitlines()))
+        result = 0
+        for chunk in data.split("\n\n"):
+            c = []
+            for line in chunk.splitlines():
+                c.append(list(map(int, (re.findall(r'\d+', line)))))
+            o = Optimize()
+            a = Int('a')
+            b = Int('b')
+            o.add(c[0][0] * a + c[1][0] * b == c[2][0])
+            o.add(c[0][1] * a + c[1][1] * b == c[2][1])
+            o.minimize(3*a+b)
+            if o.check() == z3.sat:
+                m = o.model()
+                result += 3*(m[a].as_long()) + (m[b].as_long())
+        return result
 
     def b(data):
         """Part b."""
         print('\n'.join(data.splitlines()))
+        result = 0
+        for chunk in data.split("\n\n"):
+            c = []
+            for line in chunk.splitlines():
+                c.append(list(map(int, (re.findall(r'\d+', line)))))
+            o = Optimize()
+            a = Int('a')
+            b = Int('b')
+            o.add(c[0][0] * a + c[1][0] * b == c[2][0] + C)
+            o.add(c[0][1] * a + c[1][1] * b == c[2][1] + C)
+            o.minimize(3*a+b)
+            if o.check() == z3.sat:
+                m = o.model()
+                result += 3*(m[a].as_long()) + (m[b].as_long())
+        return result
 
     puzzle = Puzzle(year=YEAR, day=DAY)
 
